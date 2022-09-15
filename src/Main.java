c1d00ca48e21dce7c5ef1d8988b9226b7e6e208d
Viktorryy <viktory_o@rambler.ru>
2022-09-15 16:29:56 +0400

    Условный оператор, часть 2
    задание 1
    
    Вопрос:каким оператором воспользоваться, чтобы получить тип операционной системы и года выпуска???

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,42 @@
 public class Main {
     public static void main(String[] args) {
-
+        //Условный оператор, часть 2
+        //
+        //задание 1
 
         String imOS;
+        int clienYear;
 //каким оператором воспользоваться, чтобы получить тип операционной системы???
-        imOS="iOS"; // считаем, что этот оператор вернул значение ))
+
+        // возвращаемые значения
+        imOS="Android"; // считаем, что этот оператор вернул значение ))
+        clienYear = 2021;
+
 
         byte clientOS;
+        int clientDeviceYear = 2015;
         String im1 = "iOS";
         String im2 = "Android";
 
 
         if (imOS == im1) {
             clientOS = 0;
-            System.out.println("Установите версию приложения для iOS по ссылке");
+            if (clienYear >= clientDeviceYear) {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            }
         }
-        if (imOS == im2) { clientOS = 1;
-            System.out.println("Установите версию приложения для Android по ссылке");
+
+        if (imOS == im2) {
+            clientOS = 1;
+            if (clienYear >= clientDeviceYear) {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+            else { System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
+
+
         }
 
 
