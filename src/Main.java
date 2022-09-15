f0c000bccfa1eb295dbd260db436eadbd7740d12
Viktorryy <viktory_o@rambler.ru>
2022-09-15 16:12:45 +0400

    Условный оператор, часть 2
    задание 1
    
    Вопрос:каким оператором воспользоваться, чтобы получить тип операционной системы???

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,23 @@ public class Main {
     public static void main(String[] args) {
 
 
+        String imOS;
+//каким оператором воспользоваться, чтобы получить тип операционной системы???
+        imOS="iOS"; // считаем, что этот оператор вернул значение ))
+
+        byte clientOS;
+        String im1 = "iOS";
+        String im2 = "Android";
+
+
+        if (imOS == im1) {
+            clientOS = 0;
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+        if (imOS == im2) { clientOS = 1;
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+
 
     }
 }
