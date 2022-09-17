82ebfa2daa113e311a6de86b22eda33367d65911
Viktorryy <viktory_o@rambler.ru>
2022-09-17 17:19:06 +0400

    Условный оператор, часть 2
    задание 3
    
    изменила

--- src/Main.java
+++ src/Main.java
@@ -5,30 +5,22 @@ public class Main {
         //
         //задание 3
 
-        int year = 2031;
+        int year = 2020;
         int y4;
         int y100;
         int y400;
         y4 = year % 4;
+        y100 = year % 100;
+        y400 = year % 400;
 
-
-        if (y4 == 0) {
-            y100 = year % 100;
-            y400 = year % 400;
-
-            if (y100 != 0) {
-                System.out.println(year + " год является высокосным");
+        if (y4 == 0 && y100 != 0 || y400 == 0) {
+            System.out.println(year + " год является высокосным");
             }
-            else if (y100 == 0 && y400 == 0) {
-                System.out.println(year + " год является высокосным");
-                }
+
                 else {
                     System.out.println(year + " год неявляется высокосным");
                 }
-        }
-        else {
-            System.out.println(year + " год неявляется высокосным");
-        }
+
 
 
 
